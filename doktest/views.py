47e002128ae485dabe46b63042bb9a0e12b494fe
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2025-02-09 18:37:00 +0300

    add context

--- doktest/views.py
+++ doktest/views.py
@@ -3,3 +3,9 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "doktest/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["python_version"] = "3.12"
+        context["django_version"] = "5.1.6"
+        return context
